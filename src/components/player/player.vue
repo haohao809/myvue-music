84d1f9834cdab8dc09291b016c814521a050203e
haohao809 <haohao809@126.com>
2017-09-22 12:13:40 +0800

    lyric

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -25,6 +25,17 @@
 						</div>
 				</div>
 				</div>
+				<div class="middle-r" ref="lyricList" :data="currentLyric && currentLyric.lines">
+					<div class="lyric-wrapper">
+						<div v-if="currentLyric">
+							<p ref="lyricLine"
+								class="text"
+								v-for="(line,index) in currentLyric.lines"
+								>{{line.txt}}</p>
+						</div>
+					</div>
+					
+				</div>
 			
 			</div>
 			<div class="bottom">
@@ -92,12 +103,17 @@
 	import ProgressCircle from 'base/progress-circle/progress-circle'
 	import {playMode} from 'common/js/config'
 	import {shuffle} from 'common/js/util'
+	import Lyric from 'lyric-parser'
 	export default{
 		data(){
 			return {
 				songReady: false,
 				currentTime: 0,
-				radius:32
+				radius:32,
+				currentLyric: null,
+				currentLineNum: 0,
+				currentShow: 'cd',
+				playingLyric: '',
 			}
 		},
 		components:{
@@ -346,7 +362,28 @@
 			 getLyric(){
 			 	this.currentSong.getLyric().then((lyric) => {
 			 		console.log(lyric);
+			 		if(this.currentSong.lyric != lyric){
+			 			return
+			 		}
+			 		this.currentLyric = new Lyric(lyric,this.handleLyric)
+			 	}).catch(()=>{
+			 		this.currentLyric = null
+			 		this.playingLyric = ''
+			 		this.currentLineNum = 0
 			 	})
+			 },
+			 handleLyric({lineNum,txt}){
+			 	if(!this.$refs.lyricLine){
+			 		return
+			 	}
+			 	this.currentLIneNum = lineNum;
+			 	if(lineNum > 5){
+			 		let lineEl = this.$refs.lyricLine[lineNum-5];
+			 		this.$refs.lyricList.scrollToElement(lineEl,1000)
+			 	}else{
+			 		this.$refs.lyricList.scrollTo(0,0,1000)
+			 	}
+			 	this.playingLyric = txt
 			 }
 		}
 	}
