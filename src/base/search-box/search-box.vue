fe4dbcfbcd7ae6c013044d8621e5ca50511a576b
haohao809 <haohao809@126.com>
2017-10-06 12:33:06 +0800

    search

---
+++ src/base/search-box/search-box.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="search-input">
+		<i class='icon-search'></i>
+		<input :placeholder="placeholder"/>
+	</div>
+</template>
+
+<script>
+
+</script>
+
+<style lang='scss' scoped>
+	.search-input{
+		text-align: center;
+		background: #333;
+		width: 100%;
+		height: 40px;
+		.icon-search{
+			font-size: 24px;
+			color: #222;
+			padding-top: 10px;
+			display: inline-block;
+		}
+		input{
+			background: #333;
+			color: #fff;
+			margin: 0 5px;
+			outline: none;		
+		}
+	}
+</style>
--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,8 +1,22 @@
 <template>
+	<div class="search">
+		<div class="search-box">
+			<search-box></search-box>
+		</div>
+	</div>
 </template>
 
 <script>
+	import SearchBox from 'base/search-box/search-box'
+	export default {
+		components:{
+			SearchBox
+		}
+	}
 </script>
 
-<style>
+<style lang='scss' scoped>
+	.search-box{
+		margin: 20px;
+	}
 </style>
