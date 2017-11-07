36007361447cff424f13e7ffb32e81007656d362
haohao809 <haohao809@126.com>
2017-11-07 22:57:46 +0800

    switches

--- src/base/switches/switches.vue
+++ src/base/switches/switches.vue
@@ -1,6 +1,8 @@
 <template>
-	<ul>
-		<li v-for="(item,index) in switches"><span>{{item}}</span></li>
+	<ul class="switches">
+		<li v-for="(item,index) in switches" class="item" 
+		:class="{'active':currentIndex===index}" @click.stop="selectItem(index)">
+		<span>{{item}}</span></li>
 	</ul>
 </template>
 
@@ -8,11 +10,35 @@
 	export default{
 		data(){
 			return{
-				switches:['最近播放','搜索历史']
+				switches:['最近播放','搜索历史'],
+				currentIndex:0
+			}
+		},
+		methods:{
+			selectItem(index){
+				this.currentIndex = index;
 			}
 		}
+
 	}
 </script>
 
-<style>
+<style scoped>
+	.switches{
+		display: flex;
+		width: 240px;
+		margin: 0 auto;
+		border: 1px solid #333;
+		border-radius: 5px;
+	}
+	.item{
+		flex:1;
+		padding: 8px;
+		text-align: center;
+		font-size: 14px;
+		color: rgba(255,255,255,0.3);
+	}
+	.active{
+		color: #FFF;
+	}
 </style>
