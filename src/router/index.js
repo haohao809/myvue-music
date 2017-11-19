f8374914a3161ae686615d362554cbaf8a85a83f
haohao809 <haohao809@126.com>
2017-11-19 15:09:48 +0800

    router-asy

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 
-import Recommend from '@/components/recommend/recommend'
-import Rank from '@/components/rank/rank'
-import Singer from '@/components/singer/singer'
-import Search from '@/components/search/search'
-import User from '@/components/user/user'
-import Disc from '@/components/disc/disc'
-import SingerDetail from '@/components/singer-detail/singer-detail'
-import TopList from '@/components/top-list/top-list'
+
+//import Recommend from '@/components/recommend/recommend'
+//import Rank from '@/components/rank/rank'
+//import Singer from '@/components/singer/singer'
+//import Search from '@/components/search/search'
+//import User from '@/components/user/user'
+//import Disc from '@/components/disc/disc'
+//import SingerDetail from '@/components/singer-detail/singer-detail'
+//import TopList from '@/components/top-list/top-list'
+//实现路由懒加载
+const Recommend = () => import('components/recommend/recommend')
+const Rank = () => import('components/rank/rank')
+const Singer = () => import('components/singer/singer')
+const Search = () => import('components/search/search')
+const User = () => import('components/user/user')
+const Disc = () => import ('components/disc/disc')
+const SingerDetail = () => import('components/singer-detail/singer-detail')
+const TopList =() => import('components/top-list/top-list')
+
 Vue.use(Router)
 
 export default new Router({
